fff5c4d83cb8f029ec1ce30dfe027667e544e8eb
enyil <enyil@github.com>
2021-11-19 13:52:30 -0600

    initial

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = (app) => {
   // Your code here
   app.log.info("Yay, the app was loaded!");
 
-  app.on("issues.opened", async (context) => {
+  app.on("repository.created", async (context) => {
     const issueComment = context.issue({
       body: "Thanks for opening this issue!",
     });
