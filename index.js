d16f8d3271623519d43e34834e0131e4d804b352
enyil <enyil@github.com>
2021-11-19 14:45:47 -0600

    updating functionality

--- index.js
+++ index.js
@@ -6,39 +6,39 @@ module.exports = (app) => {
   // Your code here
   app.log.info("Yay, the app was loaded!");
 
-  app.on("repository.created", async (context) => {
+  // app.on("repository.created", async (context) => {
 
-    const team_name = "team_name"
+  //   const team_name = "team_name"
 
-    context.octokit.teams.addOrUpdateRepoPermissionsInOrg({
-      org: "github",
-      team_slug: team_name + "_admin",
-      repo: repo.name,
-      permission: "admin"
-    });
+  //   context.octokit.teams.addOrUpdateRepoPermissionsInOrg({
+  //     org: "github",
+  //     team_slug: team_name + "_admin",
+  //     repo: repo.name,
+  //     permission: "admin"
+  //   });
 
-    context.octokit.teams.addOrUpdateRepoPermissionsInOrg({
-      org: "github",
-      team_slug: team_name + "_read",
-      repo: repo.name,
-      permission: "pull"
-    });
+  //   context.octokit.teams.addOrUpdateRepoPermissionsInOrg({
+  //     org: "github",
+  //     team_slug: team_name + "_read",
+  //     repo: repo.name,
+  //     permission: "pull"
+  //   });
 
-    context.octokit.teams.addOrUpdateRepoPermissionsInOrg({
-      org: "github",
-      team_slug: team_name + "_write",
-      repo: repo.name,
-      permission: "push"
-    });
-  });
+  //   context.octokit.teams.addOrUpdateRepoPermissionsInOrg({
+  //     org: "github",
+  //     team_slug: team_name + "_write",
+  //     repo: repo.name,
+  //     permission: "push"
+  //   });
+  // });
 
   // Sample action 
-  // app.on("issues.opened", async (context) => {
-  //   const issueComment = context.issue({
-  //     body: "Thanks for opening this issue!",
-  //   });
-  //   return context.octokit.issues.createComment(issueComment);
-  // });
+  app.on("issues.opened", async (context) => {
+    const issueComment = context.issue({
+      body: "Thanks for opening this issue!",
+    });
+    return context.octokit.issues.createComment(issueComment);
+  });
 
   // For more information on building apps:
   // https://probot.github.io/docs/
