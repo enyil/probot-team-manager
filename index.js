3d44d83e1cb59c8cdc12ae475f8bd3157ffa98c6
enyil <enyil@github.com>
2021-11-19 14:45:12 -0600

    adding team creation

--- index.js
+++ index.js
@@ -7,11 +7,38 @@ module.exports = (app) => {
   app.log.info("Yay, the app was loaded!");
 
   app.on("repository.created", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+
+    const team_name = "team_name"
+
+    context.octokit.teams.addOrUpdateRepoPermissionsInOrg({
+      org: "github",
+      team_slug: team_name + "_admin",
+      repo: repo.name,
+      permission: "admin"
     });
-    return context.octokit.issues.createComment(issueComment);
+
+    context.octokit.teams.addOrUpdateRepoPermissionsInOrg({
+      org: "github",
+      team_slug: team_name + "_read",
+      repo: repo.name,
+      permission: "pull"
+    });
+
+    context.octokit.teams.addOrUpdateRepoPermissionsInOrg({
+      org: "github",
+      team_slug: team_name + "_write",
+      repo: repo.name,
+      permission: "push"
+    });
   });
+
+  // Sample action 
+  // app.on("issues.opened", async (context) => {
+  //   const issueComment = context.issue({
+  //     body: "Thanks for opening this issue!",
+  //   });
+  //   return context.octokit.issues.createComment(issueComment);
+  // });
 
   // For more information on building apps:
   // https://probot.github.io/docs/
